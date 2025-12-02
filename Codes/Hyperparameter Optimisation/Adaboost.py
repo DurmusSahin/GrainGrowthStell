4752c26bedef5b71adc6856bfe2d934aca52a264
DurmusSahin <durmus.sahin@bil.omu.edu.tr>
2025-12-02 05:37:49 +0300

    Update Adaboost.py

--- Codes/Hyperparameter Optimisation/Adaboost.py
+++ Codes/Hyperparameter Optimisation/Adaboost.py
@@ -1,1 +1,39 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
+dataset = pd.read_excel('steel-data.xlsx')
+X = dataset.iloc[:, 0:24].values
+Y = dataset.iloc[:, 24].values
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 5)
+
+from sklearn.ensemble import AdaBoostRegressor
+adaboost_reg = AdaBoostRegressor()
+
+param_grid = {
+    'n_estimators': [50, 100, 150, 200, 225, 250], 
+    'learning_rate': [0.001, 0.01, 0.1, 1]      
+}
+
+
+grid_search = GridSearchCV(estimator=adaboost_reg, param_grid=param_grid, cv=5, scoring='r2', n_jobs=-1)
+
+
+grid_search.fit(X_train, Y_train)
+
+
+print("The best parameters:", grid_search.best_params_)
+
+
+best_model = grid_search.best_estimator_
+Y_pred_adaboost = best_model.predict(X_test)
+
+mae = mean_absolute_error(y_true = Y_test, y_pred = Y_pred_adaboost)
+mse = mean_squared_error(y_true = Y_test, y_pred = Y_pred_adaboost)
+rmse = mse ** 0.5
+r2 = r2_score(y_true = Y_test, y_pred = Y_pred_adaboost)
+
+print("MAE of AdaBoost Regressor Algorithm = ", mae)
+print("RMSE of AdaBoost Regressor Regression Algorithm = ", rmse)
+print("R^2 of AdaBoost Regressor Regression Algorithm = ", r2)
